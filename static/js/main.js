08d45ad225335a0f2821f9f2fba9771c81e51e55
hita <hitalbarracin@gmail.com>
2025-02-21 22:15:31 +0100

    Static web

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+// Initialize Feather Icons
+feather.replace();
+
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+});
+
+/* Rest of the JavaScript content remains the same */
